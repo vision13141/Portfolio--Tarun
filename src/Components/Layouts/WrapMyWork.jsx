7f99b288f303a2d563bd0b4a561718489560048a
vision13141 <s.vision.dev@gmail.com>
2024-11-02 06:59:28 +0600

    first

--- src/Components/Layouts/WrapMyWork.jsx
+++ src/Components/Layouts/WrapMyWork.jsx
@@ -1,5 +1,5 @@
 import WorkCard from "../PageCards/WorkCard";
-import Work1 from '../../assets/work1.png';
+import MWork1 from '../../assets/mwork1.png';
 import Work2 from '../../assets/work2.png';
 import Flex from '../Flex'
 
@@ -8,12 +8,12 @@ const WrapMyWork = () => {
     return (
         <div className="max-w-[980px] ml-20">
             <Flex className='gap-x-4'>
-            <WorkCard img={Work1} text="Motion Graphics" Btext="Website" />
+            <WorkCard img={MWork1} text="Motion Graphics" Btext="Website" />
             <WorkCard img={Work2} text="E-Learning" Btext="IOS App" />
             </Flex>
-            
+
             <Flex className='gap-x-4'>
-            <WorkCard img={Work1} text="Web Application" Btext="Desktop" />
+            <WorkCard img={MWork1} text="Web Application" Btext="Desktop" />
             <WorkCard img={Work2} text="Visual Design" Btext="Desktop" />
             </Flex>
         </div>
